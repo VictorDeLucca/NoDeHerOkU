dc6f7c6724a8e786d289520d3231b9c707efc3ce
Victor DeLucca <victordluk@gmail.com>
2022-08-06 02:21:01 -0300

    ok

---
+++ server.js
@@ -0,0 +1,16 @@
+const bot = require("./bot");
+
+const express = reqyure("express");
+const bot = require("./bot");
+const app = express();
+
+app.get("/", async (req, res) => {
+  const response = await bot();
+  
+  res.send(response);
+});
+
+app.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`Listening on Port ${PORT}...`);
+});
